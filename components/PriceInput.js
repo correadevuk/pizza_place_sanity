2275ce9d11caf756bcc268c7684a47a47735e720
Vitor Correa <vitorcorrea@binaryforge.io>
2020-11-10 19:24:26 +0000

    custom component on cms

---
+++ components/PriceInput.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import PatchEvent, {set, unset} from 'part:@sanity/form-builder/patch-event'
+
+
+function createPatchFrom(value){
+    return PatchEvent.from(value === "" ? unset() : set(Number(value)))
+}
+
+const formatPrice = Intl.NumberFormat('en-GB', {
+    style: 'currency',
+  currency: 'GBP',
+}).format
+
+export default function PriceInput({ type, value, onChange, inputComponent }) {
+  return (
+    <div>
+      <h2>{type.title} - {value ? formatPrice(value / 100) : ''}</h2>
+      <p>{type.description}</p>
+    <input type={type.name} value={value} onChange={event => onChange(createPatchFrom(event.target.value))} ref={inputComponent}/>
+    </div>
+  );
+}
+
+
+PriceInput.focus = function(){
+    this._inputElement.focus()
+}
--- schemas/pizza.js
+++ schemas/pizza.js
@@ -1,73 +1,75 @@
-import { MdLocalPizza as icon } from 'react-icons/md'
+import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
-    //computer name
-    name: 'pizza',
-    //visible title
-    title: 'Pizzas',
-    type: 'document',
-    // icon: () => '🍕',
-    icon,
-    fields: [
-        {
-            name: 'name',
-            title: 'Pizza Name',
-            type: 'string',
-            description: 'Name of the pizza'
-        },
-        {
-            name: 'slug',
-            title: 'Slug',
-            type: 'slug',
-            options: {
-                source: 'name',
-                maxLength: 100,
-            }
-        },
-        {
-            name: 'image',
-            title: 'Image',
-            type: 'image',
-            options: {
-                hotspot: true,
-            }
-        },
-        {
-            name: 'price',
-            title: 'Price',
-            type: 'number',
-            description: 'Price of the pizza in cents',
-            validation: Rule => Rule.min(1000).max(50000)
-        },
-       {
-           name: 'toppings',
-           title: 'Toppings',
-           type: 'array',
-           of: [{type: 'reference', to: [{type: 'topping'}]}]
-       }
-    ],
-    preview: {
-        select: {
-            title: 'name',
-            media: 'image',
-            topping0: 'toppings.0.name',
-            topping1: 'toppings.1.name',
-            topping2: 'toppings.2.name',
-            topping3: 'toppings.3.name',
-        },
-        prepare: ({title, media, ...toppings}) => {
-            // console.log(fields)
-            // 1. Filter undefined toppings out
-            // const tops = Object.values(toppings).filter(topping => topping !== undefined)
-            //Undefined === false
-            const tops = Object.values(toppings).filter(Boolean)
-            // 2. return the preview object for the pizza
-            return{
-                title,
-                media,
-                subtitle: tops.join(', ')
-            }
-            
-        }
-    }
-}
+  // computer name
+  name: 'pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  // icon: () => '🍕',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      // By importing my own component, default preview from Sanity is disabled.
+      inputComponent: PriceInput,
+      validation: (Rule) => Rule.min(1000).max(50000),
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // console.log(fields)
+      // 1. Filter undefined toppings out
+      // const tops = Object.values(toppings).filter(topping => topping !== undefined)
+      // Undefined === false
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
+};
